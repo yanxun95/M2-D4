f7d76f29a7557800f276f2125f83830bb47ea060
BananaKun <yanxun951224@gmail.com>
2021-07-08 22:03:25 +0100

    resets features done

--- index.js
+++ index.js
@@ -37,22 +37,30 @@ const numTeam = () => {
         div.appendChild(par);
         div.appendChild(ol);
         team.appendChild(div);
+        for(let i = 0; i < numberOfTeam; i++){
+            teamArr[i] = [];
+        }
     }
+    document.getElementById("numberOfTeam").value = null;
 }
 
 const assignTeam = () => {
     let totalStudent = studentArr.length;
     if(totalStudent !== 0){
-        let eachStudentPerTeam = totalStudent/numberOfTeam;
-        console.log("second:" + parseInt(eachStudentPerTeam))
-        
+        let eachStudentPerTeam = (totalStudent/numberOfTeam)+1;  
         let randomStudent = Math.floor(Math.random() * totalStudent);
         let studentToTeam = studentArr[randomStudent];
 
         let randomTeam = Math.floor(Math.random() * numberOfTeam) + 1;
+
+        if(teamArr[randomTeam-1].length <= eachStudentPerTeam){
+            teamArr[randomTeam-1].push(studentToTeam);
+        }else{
+            // recreate random number 
+        }
+        
         let randomColumn = "col"+randomTeam
         let randomOl = "ol"+randomTeam
-
         let createLi = document.getElementById(randomColumn);
         let ol = document.getElementById(randomOl);
         let li = document.createElement("li");
@@ -65,10 +73,17 @@ const assignTeam = () => {
 
         deleteTable.innerHTML = "";
         listStudent();
-
     }else{
         alert("There are no more student in waiting list.");
     }
 }
 
-// const 
+const reset = () => {
+    studentArr = [];
+    teamArr = [];
+    numberOfTeam = 0;
+    let deleteWaitingList = document.getElementById("allStudent");
+    let deleteTeam = document.getElementById("team");
+    deleteWaitingList.innerHTML = "";
+    deleteTeam.innerHTML = "";
+}
